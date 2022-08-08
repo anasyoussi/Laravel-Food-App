de596374dbd6f253c4bcb2847aec4e5bf455655f
anasyoussi <18307320+anasyoussi@users.noreply.github.com>
2022-08-08 12:26:53 +0100

    Revert "Revert "Add Form with tailwindcss and Html <no functionality yet>""
    This reverts commit d054afe7818d318a460161f744bf6c63e8b7b2cb.

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,44 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            
+                <div class="flex justify-end m-2 p-2">
+                    <a href="{{ route('admin.categories.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Category Index</a>
+                </div>
+
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form enctype="multipart/form-data">
+                        <div class="sm:col-span-6 col-span-12">
+                            <label for="title" class="block text-sm font-medium text-gray-700"> Category Name </label>
+                            <div class="mt-1">
+                                <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="title" class="block text-sm font-medium text-gray-700"> Category Description </label>
+                            <div class="mt-1">
+                                <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="sm:col-span-6 mt-2">
+                            <label for="title" class="block text-sm font-medium text-gray-700"> Post Image </label>
+                            <div class="mt-1">
+                                <input type="file" id="image" wire:model="newImage" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                        </div>
+                        <div class="flex justify-end m-2 p-2">
+                            <button type="submit" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">Add</button>
+                        </div>
+                         
+                    </form>
+                </div>
+
+        </div>
+    </div>
+</x-admin-layout>
